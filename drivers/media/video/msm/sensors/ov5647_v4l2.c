c58a349b75b2b75c4f892bbaa574fe069f287f0d
Krupal Divvela <kdivvela@codeaurora.org>
2013-02-14 17:11:08 +0530

    msm: camera: reduce ov5647 latency by using new setting
    (cherry picked from commit 0a80b1e8d340f9a0bbac3784809b2969e9f01d4c)
    
    Change-Id: I828e7e1e0840b07b2f6fadabea271cd01ad5c56a
    Signed-off-by: Krupal Divvela <kdivvela@codeaurora.org>

--- drivers/media/video/msm/sensors/ov5647_v4l2.c
+++ drivers/media/video/msm/sensors/ov5647_v4l2.c
@@ -22,11 +22,13 @@ static struct msm_sensor_ctrl_t ov5647_s_ctrl;
 DEFINE_MUTEX(ov5647_mut);
 
 static struct msm_camera_i2c_reg_conf ov5647_start_settings[] = {
-	{0x4202, 0x00},  /* streaming on */
+	{0x301c, 0xf8},  /* streaming on*/
+	{0x301a, 0xf0},  /* streaming on*/
 };
 
 static struct msm_camera_i2c_reg_conf ov5647_stop_settings[] = {
-	{0x4202, 0x0f},  /* streaming off*/
+	{0x301a, 0xf1},  /* streaming off*/
+	{0x301c, 0xfc},  /* streaming off*/
 };
 
 static struct msm_camera_i2c_reg_conf ov5647_groupon_settings[] = {
@@ -683,14 +685,11 @@ static struct v4l2_subdev_ops ov5647_subdev_ops = {
 
 int32_t ov5647_sensor_power_down(struct msm_sensor_ctrl_t *s_ctrl)
 {
-	struct msm_camera_sensor_info *info = NULL;
+	struct msm_camera_sensor_info *info = s_ctrl->sensordata;
 	unsigned short rdata;
 	int rc;
 
-	info = s_ctrl->sensordata;
-	msm_camera_i2c_write(s_ctrl->sensor_i2c_client,
-		0x4202, 0xf,
-		MSM_CAMERA_I2C_BYTE_DATA);
+	s_ctrl->func_tbl->sensor_stop_stream(s_ctrl);
 	msleep(20);
 	rc = msm_camera_i2c_read(s_ctrl->sensor_i2c_client, 0x3018,
 			&rdata, MSM_CAMERA_I2C_WORD_DATA);
@@ -740,15 +739,7 @@ int32_t ov5647_sensor_setting(struct msm_sensor_ctrl_t *s_ctrl,
 	int32_t rc = 0;
 	static int csi_config;
 	s_ctrl->func_tbl->sensor_stop_stream(s_ctrl);
-	if (csi_config == 0 || res == 0)
-		msleep(66);
-	else
-		msleep(266);
 
-	msm_camera_i2c_write(
-			s_ctrl->sensor_i2c_client,
-			0x4800, 0x25,
-			MSM_CAMERA_I2C_BYTE_DATA);
 	if (update_type == MSM_SENSOR_REG_INIT) {
 		CDBG("Register INIT\n");
 		s_ctrl->curr_csi_params = NULL;
@@ -780,11 +771,7 @@ int32_t ov5647_sensor_setting(struct msm_sensor_ctrl_t *s_ctrl,
 			0x100, 0x1,
 			MSM_CAMERA_I2C_BYTE_DATA);
 		}
-		msm_camera_i2c_write(
-			s_ctrl->sensor_i2c_client,
-			0x4800, 0x4,
-			MSM_CAMERA_I2C_BYTE_DATA);
-		msleep(266);
+
 		if (res == MSM_SENSOR_RES_4)
 			v4l2_subdev_notify(&s_ctrl->sensor_v4l2_subdev,
 					NOTIFY_PCLK_CHANGE,
