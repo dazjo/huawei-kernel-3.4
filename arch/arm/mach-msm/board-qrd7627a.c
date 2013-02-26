6178adc9db0bbd676e4e7f4da957343f21322c2f
Rajani Davuluri <rdavul@codeaurora.org>
2013-02-26 18:08:29 +0530

    msm: 7627a: Increase ADSP heap size to 19MB
    ADSP heap memory is increased from 18MB to 19MB to support
    720P video playback suspend/resume.
    
    Change-Id: Ie98cb4ac683931ddbf8c84ffd0c5fc2f573e6b6c
    
    CRs-Fixed: 455603
    Signed-off-by: Rajani Davuluri <rdavul@codeaurora.org>

--- arch/arm/mach-msm/board-qrd7627a.c
+++ arch/arm/mach-msm/board-qrd7627a.c
@@ -132,7 +132,7 @@ static struct msm_i2c_platform_data msm_gsbi1_qup_i2c_pdata = {
 
 #ifdef CONFIG_ARCH_MSM7X27A
 #define MSM_PMEM_MDP_SIZE       0x2300000
-#define MSM_PMEM_ADSP_SIZE      0x1200000
+#define MSM_PMEM_ADSP_SIZE      0x1300000
 #define CAMERA_ZSL_SIZE		(SZ_1M * 60)
 
 #ifdef CONFIG_ION_MSM
