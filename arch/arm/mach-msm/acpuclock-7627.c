cc09e1366a88a7bc51ed2aab3ca7ab7c0a5fb4ed
moddingg33k <moddingg33k@t-online.de>
2014-05-13 20:54:03 +0200

    acpuclock-7627.c: fixing table selection in select_freq_plan()
    In the function 'select_freq_plan()' there's a check to select
    the correct table for 1008Mhz CPU's, because there's a conflict
    between 7627AA and 8625 chips.
    
    However this check is wrong:
    	if (pll_mhz[ACPU_PLL_2] == 245)
    
    It should be:
    	if (pll_mhz[ACPU_PLL_1] == 245)
    
    We don't want to check PLL_2 but PLL_1 being '245' or not.
    
    Due of this bug the wrong frequency table is getting selected
    for 1GHz 8625 chips with PLL_2=245.
    
    Previously-detected-by: Peter Jung <peter.jung@lge.com>
    Signed-off-by: moddingg33k <moddingg33k@t-online.de>

--- arch/arm/mach-msm/acpuclock-7627.c
+++ arch/arm/mach-msm/acpuclock-7627.c
@@ -321,10 +321,11 @@ static struct clkctl_acpu_speed pll0_960_pll1_196_pll2_1200_pll4_1401[] = {
 };
 
 /* 8625 PLL4 @ 1008MHz with GSM capable modem */
+/* Y300-100 */
 static struct clkctl_acpu_speed pll0_960_pll1_245_pll2_1200_pll4_1008_2p0[] = {
 	{ 0, 19200, ACPU_PLL_TCXO, 0, 0, 2400, 3, 0, 30720 },
 	{ 0, 61440, ACPU_PLL_1, 1, 3,  7680, 3, 0, 61440 },
-	{ 0, 122880, ACPU_PLL_1, 1, 1,  15360, 3, 1, 61440 },
+	{ 1, 122880, ACPU_PLL_1, 1, 1,  15360, 3, 1, 61440 },
 	{ 1, 245760, ACPU_PLL_1, 1, 0, 30720, 3, 1, 61440 },
 	{ 0, 300000, ACPU_PLL_2, 2, 3, 37500, 3, 2, 122880 },
 	{ 1, 320000, ACPU_PLL_0, 4, 2, 40000, 3, 2, 122880 },
@@ -1027,7 +1028,7 @@ static void __devinit select_freq_plan(void)
 	 * are using different clock plan based reprogramming method.
 	 */
 	if (cpu_is_msm8625() &&	pll_mhz[ACPU_PLL_4] == 1008) {
-		if (pll_mhz[ACPU_PLL_2] == 245)
+		if (pll_mhz[ACPU_PLL_1] == 245)
 			acpu_freq_tbl =
 				pll0_960_pll1_245_pll2_1200_pll4_1008_2p0;
 		else
